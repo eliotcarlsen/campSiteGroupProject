b78ee2b401689dbcf0c033f54d66ffaccc296f16
Xing Li <msuli_1120@ymail.com>
2017-05-17 09:09:16 -0700

    add weather

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,15 +4,19 @@ import { Http } from '@angular/http';
 import { GoogleMapService } from '../google-map.service';
 import { CampInfoDataService } from '../camp-info-data.service';
 import { routing } from '../app.routing';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
-  providers: [GoogleMapService, CampInfoDataService],
+  providers: [GoogleMapService, CampInfoDataService, WeatherService],
 })
 export class MapComponent implements OnInit {
   //  position: string = this.lat + ',' + this.lng;
+  cityName;
+  dayTemp;
+  eveTemp;
   geo;
   title;
   direction;
@@ -20,7 +24,7 @@ export class MapComponent implements OnInit {
   positions = [];
   facilities = [];
   facilitiesLatLng = [];
-  constructor(private http: Http, private apikey: ApiKey, private googlemapservice: GoogleMapService, private campInfoDataService: CampInfoDataService) { }
+  constructor(private http: Http, private apikey: ApiKey, private googlemapservice: GoogleMapService, private campInfoDataService: CampInfoDataService, private weatherService: WeatherService) { }
 
   ngOnInit() {
   }
@@ -29,6 +33,11 @@ export class MapComponent implements OnInit {
     this.location = place;
     this.googlemapservice.search(place).subscribe((data) => {
       this.geo = {lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng};
+      this.weatherService.getWeather(data.results[0].geometry.location.lat,    data.results[0].geometry.location.lng).subscribe((weather) => {
+        this.cityName = weather.city.name;
+        this.dayTemp = ((weather.list[0].temp.day - 273) * 9 / 5 + 32).toFixed(0);
+        this.eveTemp = ((weather.list[0].temp.eve - 273) * 9 / 5 + 32).toFixed(0);
+      });
       this.campInfoDataService.fetchCampsites(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng).subscribe((facilities) => {
         for (let i = 0; i < facilities.RECDATA.length; i++) {
           if (facilities.RECDATA[i].FacilityTypeDescription === 'Camping') {
