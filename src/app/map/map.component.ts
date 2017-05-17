d154d2d5e67dfdcd7354bd81d26a355af2dd8b46
Rob McKenzie <mckenro@gmail.com>
2017-05-17 15:36:16 -0700

    random

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -53,11 +53,7 @@ export class MapComponent implements OnInit {
     this.location = place;
     this.googlemapservice.search(place).subscribe((data) => {
       this.geo = {lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng};
-      this.weatherService.getWeather(data.results[0].geometry.location.lat,    data.results[0].geometry.location.lng).subscribe((weather) => {
-        this.cityName = weather.city.name;
-        this.dayTemp = ((weather.list[0].temp.day - 273) * 9 / 5 + 32).toFixed(0);
-        this.eveTemp = ((weather.list[0].temp.eve - 273) * 9 / 5 + 32).toFixed(0);
-      });
+
 
       this.campInfoDataService.fetchCampsites(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng).subscribe((facilities) => {
         for (let i = 0; i < facilities.RECDATA.length; i++) {
